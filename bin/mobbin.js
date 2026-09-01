8d90435ba8d94bccb91ac2e92360bd8fefdcc233
Vsevolod Trofimov <vsevolod.trofimow@ya.ru>
2026-09-01 21:33:41 +0200

    fix: invoke CLI from global executable

--- bin/mobbin.js
+++ bin/mobbin.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-import '../src/cli.js';
+import { run } from '../src/cli.js';
+
+run(process.argv.slice(2)).catch((error) => {
+  process.stderr.write(`mobbin: ${error.message}\n`);
+  process.exitCode = 1;
+});
